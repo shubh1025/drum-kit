2ce143659216c19e4e9e4a176f6eafced1b51860
shubham <shubhamaery10@gmail.com>
2018-07-28 20:41:16 +0530

    initial not working version

---
+++ script.js
@@ -0,0 +1,12 @@
+
+var button = document.getElementById('common1');
+
+button.addEventListener('click', myPlay); 
+
+
+
+function myPlay() {
+    var a = document.getElementById('A');
+    a.play();
+} ;
+
