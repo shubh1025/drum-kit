ffe9c55a90bb80a3c29bfb4cbb1b77cb3f3e5b87
shubh1025 <shubhamaery10@gmail.com>
2018-08-06 11:21:53 +0530

    inital drumkit one button working

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 
 var button = document.getElementById('common1');
 
+
 button.addEventListener('click', myPlay); 
+//button.removeEventListener('click', myPause);
 
 
 
 function myPlay() {
-    var a = document.getElementById('A');
-    a.play();
-} ;
+   
+    var a = new Audio('sounds/clap.wav');
+    a.play(); // to play audio file 
+    //this.removeEventListener('click', myPlay);
+}
+
+
+//    function myPause() 
+//    {
+///*    button.removeEventListener('click', myPause); */
+//        a.pause();
+//}
+
 
